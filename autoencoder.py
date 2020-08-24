9517304f76bc25007e17bd14247d034ce7980347
shatalinra <shatalinra@gmail.com>
2020-08-24 20:01:19 +0300

    Implemented multiple attempts at weight initialization. Improved GPU utilization and training time by using model fit method and early stopping.

--- autoencoder.py
+++ autoencoder.py
@@ -1,6 +1,29 @@
 import tensorflow as tf
 from pathlib import Path
 
+class AutoencoderLogger(tf.keras.callbacks.Callback):
+    def __init__(self, test_data, test_rate):
+        '''
+        display: Number of epochs to wait before outputting loss
+        '''
+        self.test_data = test_data
+        self.test_rate = test_rate
+        self.loss = tf.losses.MeanSquaredError()
+        self.last_loss = None
+        
+    def on_epoch_end(self, epoch, logs = None):
+         if epoch % self.test_rate != 0: return
+
+         reconstructed_data = self.model(self.test_data)
+         loss = self.loss(self.test_data, reconstructed_data)
+
+         delta = loss
+         if self.last_loss:
+             delta = self.last_loss - loss
+         self.last_loss = loss
+
+         print('Epoch {0}, loss {1:0.6f}, change {2:0.6f}'.format(epoch, loss, delta))
+
 class Autoencoder(object):
     """autoencoder for tiny object detection"""
     def __init__(self, patch_size, *args, **kwargs):
@@ -9,7 +32,8 @@ class Autoencoder(object):
         return super().__init__(*args, **kwargs)
 
     def load(self, path):
-         self._model = tf.keras.models.load_model(path + "\\v4")
+         self._model = tf.keras.models.load_model(path + "\\v1")
+         self._model.summary()
 
     def train(self, image_patches, path):
         dir = Path(path)
@@ -46,19 +70,34 @@ class Autoencoder(object):
                 except IOError:
                     pass
 
+        # use same min loss change value for training
+        min_loss_change = tf.constant(0.000001, dtype=tf.float32)
+        init_attempts = 10
+
         # now if we don't have trained first layers, train them
         if not conv1_layer:
-            model1 =  tf.keras.Sequential(name = "autoencoder-v1")
-            model1.add(tf.keras.Input(shape = [self._patch_size, self._patch_size, 3]))
-            model1.add(tf.keras.layers.Conv2D(filters=15, kernel_size = 3, strides=2, padding="same", activation = 'relu', name = "conv1"))
-            model1.add(tf.keras.layers.Conv2DTranspose(filters=3, kernel_size = 3, strides=2, padding="same", activation = 'relu', name = "deconv1"))
-            print("Training autoencoder v1")
-            self._train_model(model1, image_patches, 0.002, 1600)
-            model1.save(path+ '\\v1')
+            best_model = None
+            best_loss = tf.constant(100.0, dtype=tf.float32)
+            for init_attempt in range(init_attempts):
+                model1 =  tf.keras.Sequential(name = "autoencoder-v1")
+                model1.add(tf.keras.Input(shape = [self._patch_size, self._patch_size, 3]))
+                model1.add(tf.keras.layers.Conv2D(filters=3, kernel_size = 3, strides = 2, padding="same", activation = 'relu', name = "conv1"))
+                model1.add(tf.keras.layers.Conv2DTranspose(filters=3, kernel_size = 3, strides = 2, padding="same", activation = 'relu', name = "deconv1"))
+                print("Training autoencoder v1: attempt", init_attempt)
+                loss = self._train_model(model1, image_patches, min_loss_change, 5000)
+                if loss < best_loss:
+                    best_loss = loss
+                    best_model = model1
+
+            best_model.save(path + '\\v1')
+            print("best loss is %.6f" % best_loss)
+
+            self._model = best_model
+            return
 
             # extract layers from model
-            conv1_layer = model1.get_layer("conv1")
-            deconv1_layer = model1.get_layer("deconv1")
+            conv1_layer = best_model.get_layer("conv1")
+            deconv1_layer = best_model.get_layer("deconv1")
 
         # freeze first layers
         conv1_layer.trainable = False
@@ -66,19 +105,32 @@ class Autoencoder(object):
 
         # if we don't have trained second layers, also train them
         if not conv2_layer:
-            model2 =  tf.keras.Sequential(name = "autoencoder-v2")
-            model2.add(tf.keras.Input(shape = [self._patch_size, self._patch_size, 3]))
-            model2.add(conv1_layer)
-            model2.add(tf.keras.layers.Conv2D(filters=15, kernel_size = 3, strides=2, padding="same", activation = 'relu', name = "conv2"))
-            model2.add(tf.keras.layers.Conv2DTranspose(filters=15, kernel_size = 3, strides=2, padding="same", activation = 'relu', name = "deconv2"))
-            model2.add(deconv1_layer)
-            print("Training autoencoder v2")
-            self._train_model(model2, image_patches, 0.002, 1600)
-            model2.save(path + "\\v2")
+            best_model = None
+            best_loss = tf.constant(100.0, dtype=tf.float32)
+            for init_attempt in range(init_attempts):
+                model2 =  tf.keras.Sequential(name = "autoencoder-v2")
+                model2.add(tf.keras.Input(shape = [self._patch_size, self._patch_size, 3]))
+                model2.add(conv1_layer)
+                model2.add(tf.keras.layers.Conv2D(filters=4, kernel_size = 3, strides=2, padding="same", activation = 'relu', name = "conv2"))
+                model2.add(tf.keras.layers.Conv2DTranspose(filters=2, kernel_size = 3, strides=2, padding="same", activation = 'relu', name = "deconv2"))
+                model2.add(deconv1_layer)
+                print("Training autoencoder v1: attempt", init_attempt)
+                loss = self._train_model(model2, image_patches, min_loss_change, 5000)
+                if loss < best_loss:
+                    best_loss = loss
+                    best_model = model2
+
+            best_model.save(path + '\\v2')
+            print("best loss is %.6f" % best_loss)
+
+            self._model = best_model
+            return
 
             # extract layers from model
             conv2_layer = model2.get_layer("conv2")
             deconv2_layer = model2.get_layer("deconv2")
+
+      
 
         # freeze second layers
         conv2_layer.trainable = False
@@ -90,13 +142,16 @@ class Autoencoder(object):
             model3.add(tf.keras.Input(shape = [self._patch_size, self._patch_size, 3]))
             model3.add(conv1_layer)
             model3.add(conv2_layer)
-            model3.add(tf.keras.layers.Conv2D(filters=30, kernel_size = 3, strides=2, padding="same", activation = 'relu', name = "conv3"))
-            model3.add(tf.keras.layers.Conv2DTranspose(filters=15, kernel_size = 3, strides=2, padding="same", activation = 'relu', name = "deconv3"))
+            model3.add(tf.keras.layers.Conv2D(filters=9, kernel_size = 3, strides=2, padding="same", activation = 'relu', name = "conv3"))
+            model3.add(tf.keras.layers.Conv2DTranspose(filters=6, kernel_size = 3, strides=2, padding="same", activation = 'relu', name = "deconv3"))
             model3.add(deconv2_layer)
             model3.add(deconv1_layer)
             print("Training autoencoder v3")
-            self._train_model(model3, image_patches, 0.005, 1000)
+            self._train_model(model3, image_patches, min_loss_change)
             model3.save(path + "\\v3")
+
+            #self._model = model3
+            #return
 
             # extract layers from model
             conv3_layer = model3.get_layer("conv3")
@@ -112,39 +167,25 @@ class Autoencoder(object):
         model4.add(conv1_layer)
         model4.add(conv2_layer)
         model4.add(conv3_layer)
-        model4.add(tf.keras.layers.Conv2D(filters=18, kernel_size = 3, strides=2, padding="same", activation = 'relu', name = "conv4"))
-        model4.add(tf.keras.layers.Conv2DTranspose(filters=30, kernel_size = 3, strides=2, padding="same", activation = 'relu', name = "deconv4"))
+        model4.add(tf.keras.layers.Conv2D(filters=15, kernel_size = 3, strides=2, padding="same", activation = 'relu', name = "conv4"))
+        model4.add(tf.keras.layers.Conv2DTranspose(filters=9, kernel_size = 3, strides=2, padding="same", activation = 'relu', name = "deconv4"))
         model4.add(deconv3_layer)
         model4.add(deconv2_layer)
         model4.add(deconv1_layer)
         print("Training autoencoder v4")
-        self._train_model(model4, image_patches, 0.005, 1000)
+        self._train_model(model4, image_patches, min_loss_change)
         model4.save(path + "\\v4")
             
         self._model = model4
 
-    def _train_model(self, model, image_patches, rate, epochs):
-        train_data = tf.data.Dataset.from_tensor_slices(image_patches).batch(512).shuffle(buffer_size = 1024)
-        optimizer = tf.optimizers.RMSprop(learning_rate = rate) 
-        mse_loss = tf.keras.losses.MeanSquaredError() 
-        loss_metric = tf.keras.metrics.Mean()
-
+    def _train_model(self, model, image_patches, min_loss_change, max_epoch):
         model.summary()
-        for epoch in range(epochs):
-            # Iterate over the batches of the dataset.
-            for step, x_batch_train in enumerate(train_data):
-                with tf.GradientTape() as tape:
-                    reconstructed = model(x_batch_train)
-                    # Compute reconstruction loss
-                    loss = mse_loss(x_batch_train, reconstructed)
+        model.compile(loss=tf.losses.MeanSquaredError(), optimizer=tf.optimizers.RMSprop())
 
-                grads = tape.gradient(loss, model.trainable_weights)
-                optimizer.apply_gradients(zip(grads, model.trainable_weights))
-
-                loss_metric(loss)
-
-            if epoch % 10 == 0:
-                print("epoch %d: mean loss = %.6f" % (epoch, loss_metric.result()))
+        logging = AutoencoderLogger(image_patches, 50)
+        stopping = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=100, mode='min')
+        history = model.fit(image_patches, image_patches, 512, max_epoch, verbose=0, callbacks=[logging, stopping])
+        return history.history["loss"][-1]
 
     def __call__(self, data):
         return self._model(data)
--- tiny_object_detection.py
+++ tiny_object_detection.py
@@ -5,12 +5,19 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import autoencoder
 
-raw_file = tf.io.read_file('bolt-sample.jpg')
-image = tf.io.decode_jpeg(raw_file)
-image = tf.expand_dims(image, 0)
-image_float = tf.cast(image, dtype = tf.float32) / 255
+#tf.debugging.set_log_device_placement(True)
 
-image_size = image.get_shape().as_list()[1:3]
+train_file = tf.io.read_file('train.jpg')
+train_image = tf.io.decode_jpeg(train_file)
+train_image = tf.expand_dims(train_image, 0)
+train_image = tf.cast(train_image, dtype = tf.float32) / 255
+
+test_file = tf.io.read_file('test.jpg')
+test_image = tf.io.decode_jpeg(test_file)
+test_image = tf.expand_dims(test_image, 0)
+test_image = tf.cast(test_image, dtype = tf.float32) / 255
+
+test_image_size = test_image.get_shape().as_list()[1:3]
 patch_size = 64
 stride = 16
 
@@ -18,28 +25,30 @@ ksizes = [1, patch_size, patch_size, 1]
 strides = [1, stride, stride, 1]
 rates = [1, 1, 1, 1]
 
+train_image_patches = tf.image.extract_patches(train_image, ksizes, strides, rates, 'VALID')
+train_image_patches = tf.reshape(train_image_patches, [-1, patch_size, patch_size, 3])
 
-image_patches = tf.image.extract_patches(image_float, ksizes, strides, rates, 'VALID')
-patches_count = image_patches.get_shape().as_list()[1:3]
-image_patches = tf.reshape(image_patches, [-1, patch_size, patch_size, 3])
+test_image_patches = tf.image.extract_patches(test_image, ksizes, strides, rates, 'VALID')
+test_patches_count = test_image_patches.get_shape().as_list()[1:3]
+test_image_patches = tf.reshape(test_image_patches, [-1, patch_size, patch_size, 3])
 
 network = autoencoder.Autoencoder(patch_size)
 try:
     network.load("model")
 except IOError:
-    network.train(image_patches, "model")
+    network.train(test_image_patches, "model")
 
-reconstructed = network(image_patches)
+reconstructed = network(test_image_patches)
 
 debug = True
 if debug:
     
     fig=plt.figure(figsize=(15, 4))
-    offset = 32 * patches_count[0] + 9
+    offset = 32 * test_patches_count[0] + 9
     cols = 10
     for i in range(1, cols):
         fig.add_subplot(2, cols, i)
-        plt.imshow(image_patches[offset + i])
+        plt.imshow(test_image_patches[offset + i])
         fig.add_subplot(2, cols, cols + i)
         plt.imshow(reconstructed[offset + i])
     plt.show()
@@ -47,10 +56,10 @@ if debug:
 # search for patch with max loss
 max_loss = tf.constant(0, dtype=tf.float32)
 max_loss_pos = None
-for i in range(patches_count[1]):
-    for j in range(patches_count[0]):
-        src_patch = image_patches[i * patches_count[0] + j]
-        reconstructed_patch = reconstructed[i * patches_count[0] + j]
+for i in range(test_patches_count[1]):
+    for j in range(test_patches_count[0]):
+        src_patch = test_image_patches[i * test_patches_count[0] + j]
+        reconstructed_patch = reconstructed[i * test_patches_count[0] + j]
         diff = tf.math.abs(src_patch - reconstructed_patch)
         loss = tf.reduce_mean(diff)
         if i == 32 and j == 10:
@@ -66,13 +75,13 @@ for i in range(patches_count[1]):
             max_loss_pos = [i, j]
 
 # draw rect on src image
-x = max_loss_pos[1] / patches_count[0]
-y = max_loss_pos[0] / patches_count[1]
-width = patch_size / image_size[0]
-height = patch_size / image_size[1]
+x = max_loss_pos[1] / test_patches_count[0]
+y = max_loss_pos[0] / test_patches_count[1]
+width = patch_size / test_image_size[0]
+height = patch_size / test_image_size[1]
 box = tf.convert_to_tensor([[[x, y, x + width, y + height]]], dtype=tf.float32)
 colors = tf.convert_to_tensor([[1, 0, 0]], dtype=tf.float32)
-output = tf.image.draw_bounding_boxes(image_float, box, colors)
+output = tf.image.draw_bounding_boxes(test_image, box, colors)
 
 plt.imshow(output[0])
 plt.show()
@@ -82,7 +91,7 @@ print("max loss is", max_loss)
 
 fig=plt.figure(figsize=(15, 4))
 fig.add_subplot(1, 2, 1)
-plt.imshow(image_patches[max_loss_pos[0] * patches_count[0] + max_loss_pos[1]])
+plt.imshow(test_image_patches[max_loss_pos[0] * test_patches_count[0] + max_loss_pos[1]])
 fig.add_subplot(1, 2, 2)
-plt.imshow(reconstructed[max_loss_pos[0] * patches_count[0] + max_loss_pos[1]])
+plt.imshow(reconstructed[max_loss_pos[0] * test_patches_count[0] + max_loss_pos[1]])
 plt.show()
